aa5cff736a594ff6dade5b56417cb4a6f54c568c
lolo-pb <lpizzutobeltran@gmail.com>
2024-07-03 15:53:06 -0300

    arranca el tad

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -1,3 +1,13 @@
 /* CDT and functions to be used in parkingTicketsCHI and parkingTicketsNYC */
 
 #include "parkingTicketsADT.h"
+
+typedef struct parkingTicketsCDT{
+    //size_t size;
+    firstQ1;
+    firstQ2;
+    firstQ3;
+    char** ReferenceInfraction;
+}
+
+
