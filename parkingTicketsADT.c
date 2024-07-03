eee59668919f56e1c311f366155e50794c89fbe6
cyelmini <cyelmini@itba.edu.ar>
2024-07-03 18:08:18 -0300

    struct changes

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -2,6 +2,30 @@
 
 #include "parkingTicketsADT.h"
 
+typedef struct plate{
+    char plate[MAX_PLATE];
+    size_t plateInfractionCount;
+}TPlate;
 
+typedef struct infraction{
+    char description[MAX_DESC];
+    TPlate * vec; // Vector containing the different plates that committed the infraction
+    size_t maxPosPlate; // Position where the plate that repeated the infraction the most is stored in the vector
+    size_t infractionCount; // Total amount of times the infraction was committed
+}TInfraction;
 
+typedef struct node{
+    char agency[MAX_AG];
+    TInfraction * vec; // Vector containing the different infractions issued by the correspondent agency (each position in this vector corresponds with the infractionId)
+    size_t size; // Reserved space for the vector
+    size_t totalInfractionCount; // Total amount of infractions
+    size_t maxPosInfraction; // Position where the most repeated infraction is stored in the vector
+    struct node * tail;  // Pointer to the next agency
+} TNode;
+
+typedef struct TNode * TList;
+
+struct parkingTicketsCDT{
+    TList firstAgency;  // Pointer to the first element of the list of agencies, which is added in alphabetical order.
+};
 
--- parkingTicketsADT.h
+++ parkingTicketsADT.h
@@ -1,7 +1,12 @@
 /* header with the functions used in the ADT and defined variables */
 
-
 #ifndef TPE_PI_1Q2024_PARKINGTICKETSADT_H
 #define TPE_PI_1Q2024_PARKINGTICKETSADT_H
+
+#define MAX_PLATE 10
+#define MAX_DESC 50
+#define MAX_AG 35
+
+
 
 #endif //TPE_PI_1Q2024_PARKINGTICKETSADT_H
