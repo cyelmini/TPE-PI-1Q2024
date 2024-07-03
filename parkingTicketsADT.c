73e747b6f1344c6322801b0df2cd139c8f0c0b53
lolo-pb <lpizzutobeltran@gmail.com>
2024-07-03 16:11:54 -0300

    Update parkingTicketsADT.c

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -4,10 +4,41 @@
 
 typedef struct parkingTicketsCDT{
     //size_t size;
-    firstQ1;
-    firstQ2;
-    firstQ3;
+    struct nodoq13* firstQ1;
+    struct nodoq2* firstQ2;
+    struct nodoq13* firstQ3;
     char** ReferenceInfraction;
+}parkingTicketsCDT;
+
+typedef struct nodoq2{
+    char* AgencyName;
+    struct ndodoq2* tail;
+}nodoq2;
+
+/*
+typedef struct nodoq13{
+    char* infractionName;
+    struct ndodoq13* tailQ1;
+    struct ndodoq13* tailQ3;
+}nodoq13;
+*/
+   /// alternativa con 3 listas 
+typedef struct nodoq1{
+    char* infractionName;
+    struct ndodoq1* tailQ1;
+}nodoq1;
+
+typedef struct nodoq3{
+    char* infractionName;
+    struct ndodoq3* tailQ3;
+}nodoq3;
+
+
+
+nodoq13* addinfraccAlpha(,){
+
+
 }
+
 
 
