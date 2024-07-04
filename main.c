526ec1b3e43c99595972ab4be15dfa4a4545c924
Ftrivelloni <ftrivelloni@itba.edu.ar>
2024-07-04 02:36:52 -0300

    inicio de main
    se le agrego los argumentos de los archivos, su apertura en modo solo lectura. Ademas de programacion defensiva

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
 /* Main that calls upon parkingTicketsCHI and parkingTicketsNYC to process the correspondent data */
+#include <stdio.h>
+#include <string.h>
+#include "htmlTable.h"
+#include "parkingTicketsADT.h"
+
+int main(int argc, char * argv[]){
+    if (argc != 2){
+        fprintf(sterr, "Invalid amount of agruments\n");
+        exit(ERROR_ARG);
+    }
+    
+    FILE * fileTickets = fopen(argv[0], "rt");
+    FILE * fileInfractions = fopen(argv[1], "rt");
+
+    if(fileTickets == NULL || fileInfractions == NULL){
+        fprintf(sterr, 'Unable to open files\n');
+        exit(ERROR_OPEN);
+    }
+
+    
+
+    //parkingTicketsADT tickets = newTicket();
+    
+}
