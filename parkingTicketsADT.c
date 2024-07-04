4e48b26167717d5c730a7bbf415553649c437d13
cyelmini <cyelmini@itba.edu.ar>
2024-07-03 21:04:32 -0300

    newParking function

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -1,35 +1,7 @@
 /* CDT and functions to be used in parkingTicketsCHI and parkingTicketsNYC */
 
 #include "parkingTicketsADT.h"
-
-/* OPCION 1 (a definir) */
-typedef struct plate{
-    char plate[MAX_PLATE];
-    size_t count;
-}TPlate;
-
-typedef struct infraction{
-    char description[MAX_DESC];
-    TPlate * plates; // Vector containing the different plates that committed the infraction
-    size_t maxPosPlate; // Position where the plate that repeated the infraction the most is stored in the vector
-    size_t count; // Total amount of times the infraction was committed
-}TInfraction;
-
-typedef struct node{
-    char agency[MAX_AG];
-    TInfraction * infractions; // Vector containing the different infractions issued by the correspondent agency (each position in this vector corresponds with the infractionId)
-    size_t size; // Reserved space for the vector
-    size_t totalCount; // Total amount of infractions
-    size_t maxPosInfraction; // Position where the most repeated infraction is stored in the vector
-    struct node * tail;  // Pointer to the next agency
-} TNode;
-
-typedef TNode * TList;
-
-struct parkingTicketsCDT{
-    TList firstAgency;  // Pointer to the first element of the list of agencies, which is added in alphabetical order.
-};
-
+#include <errno.h>
 
 /* OPCION 2
 typedef struct infraction{
@@ -69,3 +41,46 @@ struct parkingTicketsCDT{
     TListInf firstInfraction; // Pointer to the first element of the list of infractions (to be used in query3)
 };
 */
+
+/* OPCION 1 (a definir) */
+typedef struct plate{
+    char plate[MAX_PLATE];
+    size_t count;
+}TPlate;
+
+typedef struct infraction{
+    char description[MAX_DESC];
+    TPlate * plates; // Vector containing the different plates that committed the infraction
+    size_t maxPosPlate; // Position where the plate that repeated the infraction the most is stored in the vector
+    size_t count; // Total amount of times the infraction was committed
+}TInfraction;
+
+typedef struct node{
+    char agency[MAX_AG];
+    TInfraction * infractions; // Vector containing the different infractions issued by the correspondent agency (each position in this vector corresponds with the infractionId)
+    size_t size; // Reserved space for the vector
+    size_t totalCount; // Total amount of infractions
+    size_t maxPosInfraction; // Position where the most repeated infraction is stored in the vector
+    struct node * tail;  // Pointer to the next agency
+} TNode;
+
+typedef TNode * TList;
+
+struct parkingTicketsCDT{
+    TList firstAgency;  // Pointer to the first element of the list of agencies, which is added in alphabetical order.
+};
+
+parkingTicketsADT newParking(void){
+    errno = OK;
+    parkingTicketsADT aux = calloc(1, sizeof(parkingTicketsCDT));
+
+    if(errno == ENOMEM || aux == NULL){
+        errno = MEMERR;
+        return NULL;
+    }
+    return aux;
+}
+
+
+
+
--- parkingTicketsADT.h
+++ parkingTicketsADT.h
@@ -3,10 +3,23 @@
 #ifndef TPE_PI_1Q2024_PARKINGTICKETSADT_H
 #define TPE_PI_1Q2024_PARKINGTICKETSADT_H
 
+#include <stdlib.h>
+
 #define MAX_PLATE 10
 #define MAX_DESC 50
 #define MAX_AG 35
 
 
+typedef struct parkingTicketsCDT * parkingTicketsADT;
+
+
+
+
+enum {OK = 0, MEMERR, TOKERR};
+
+/*
+ * MEMERR = Memory error
+ * TOKENERR = Token error
+ */
 
 #endif //TPE_PI_1Q2024_PARKINGTICKETSADT_H
