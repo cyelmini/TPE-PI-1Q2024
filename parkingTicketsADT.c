11d396b66dd6276efc29b6596b814b006cf0ca8f
lolo-pb <lpizzutobeltran@gmail.com>
2024-07-10 18:40:36 -0300

    Agrego lod query5 comentado

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -47,6 +47,18 @@ typedef struct infraction{
     size_t totalCount;
 }TInfraction;
 
+////////////////////////////////para query5 /////////////////////
+// typedef struct infraction{
+//     char description[MAX_DESC];
+//     TListPlate firstPlate;
+//     char plate[MAX_PLATE];
+//     size_t maxPlateCount;
+//     size_t totalCount;
+
+//     size_t minAmount;//se actualizan en el addTicketToAgency
+//     size_t maxAmount;
+// }TInfraction;
+
 /* List of agencies containing their names, the infractions emitted by them and the position where the most emitted
  * infraction is stored (to be used in query2)
  */
@@ -171,6 +183,7 @@ static void updateInfraction(TListAg list, size_t infractionId) {
  * by calling upon addPlateRec. If needed, updates the plate that committed the infraction the most and the most
  * popular infraction by agency.
  */
+// static void addTicketToAgency(TListAg list, const char *infractionDesc, size_t infractionId, const char *plate, size_t amount) {
 static void addTicketToAgency(TListAg list, const char *infractionDesc, size_t infractionId, const char *plate) {
 
     if(infractionId >= list->size) {
@@ -205,11 +218,21 @@ static void addTicketToAgency(TListAg list, const char *infractionDesc, size_t i
 
     // Update the most popular infraction by agency
     updateInfraction(list, infractionId);
+
+    // // Check minAmount and maxAmount ////////// para query5 ///////////////////////////////////////////
+    // if(list->infractions[infractionId].minAmount>amount){
+    //     list->infractions[infractionId].minAmount=amount;
+    // }
+    
+    // if(list->infractions[infractionId].maxAmount<amount){
+    //     list->infractions[infractionId].maxAmount=amount;
+    // }
 }
 
 /* Searches for the given agency in the list of agencies. If it does not found it, it adds it to the list. In either
  * case, it registers the new ticket calling upon addTicketToAgency.
  */
+// static TListAg addTicketRec(TListAg list, const char *agency, const char *infractionDesc, size_t infractionId, const char *plate, int *flag, size_t amount) {
 static TListAg addTicketRec(TListAg list, const char *agency, const char *infractionDesc, size_t infractionId, const char *plate, int *flag) {
     int c;
     if (list == NULL || (c = strcasecmp(list->agency, agency)) > 0) { // Agency does not exist
@@ -220,15 +243,18 @@ static TListAg addTicketRec(TListAg list, const char *agency, const char *infrac
         }
         strcpy(newAg->agency, agency);
         addTicketToAgency(newAg, infractionDesc, infractionId, plate);
+        // addTicketToAgency(newAg, infractionDesc, infractionId, plate, amount);
         newAg->tail = list;
         *flag = 1;
         return newAg;
     }
     if(c == 0) {
         addTicketToAgency(list, infractionDesc, infractionId, plate);
+        // addTicketToAgency(list, infractionDesc, infractionId, plate, amount);
         *flag = 1;
         return list;
     }
+    // list->tail = addTicketRec(list->tail, agency, infractionDesc, infractionId, plate, flag, amount);
     list->tail = addTicketRec(list->tail, agency, infractionDesc, infractionId, plate, flag);
     return list;
 }
@@ -237,6 +263,7 @@ static TListAg addTicketRec(TListAg list, const char *agency, const char *infrac
 * If the agency did not exist, it adds it to the list as well. Returns 1 if successfully done
 * and 0 if not.
 */
+// int addTicket(parkingTicketsADT p, const char *agency, size_t infractionId, const char *plate, size_t amount) {
 int addTicket(parkingTicketsADT p, const char *agency, size_t infractionId, const char *plate) {
     int flag = 0;
     if(agency == NULL || plate ==  NULL){
@@ -247,6 +274,7 @@ int addTicket(parkingTicketsADT p, const char *agency, size_t infractionId, cons
         flag = 1;
         return flag;  // If one infraction is present in the infractions.csv but not in the tickets.csv, we skip it and don't do anything
     }
+    // p->firstAgency = addTicketRec(p->firstAgency, agency, p->descriptions[infractionId], infractionId, plate, &flag, amount);
     p->firstAgency = addTicketRec(p->firstAgency, agency, p->descriptions[infractionId], infractionId, plate, &flag);
     return flag;
 }
